b8c150272181446a967d046c113e1477a642ab59
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2024-05-09 09:40:10 +0700

    partner auto active

--- app/Observers/StoreObserver.php
+++ app/Observers/StoreObserver.php
@@ -2,11 +2,14 @@
 
 namespace App\Observers;
 
+use App\Enums\StoreStatus;
 use App\Models\Store;
+use Illuminate\Support\Facades\Gate;
 
 class StoreObserver
 {
     public function creating(Store $store): void {
         $store->slug = str()->slug($store->name);
+        $store-> status = Gate::check('isPartner') ? StoreStatus::ACTIVE : StoreStatus::PENDING;
     }
 }
